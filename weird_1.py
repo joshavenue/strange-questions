db3b627d027600ce99a36c53352381d93073acd1
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-23 06:10:21 +0800

    i do not understand the logic here

---
+++ weird_1.py
@@ -0,0 +1,20 @@
+a = [0,1,2,3,4,5,6,7,8,9]
+
+print(a[5])
+print(a[a[5]])
+
+b = [1,1,2,3,5,8,13]
+
+print(b[4])
+print(b[b[4]])
+
+c = [2,5,2,5,7,2,8,4,7,9]
+
+print(c[5])
+print(c[c[5]])
+
+d = [1,1,4,6,9,12,113,1223]
+
+print(d[3])
+print(d[d[3]])
+## The outcome is really strange...##
